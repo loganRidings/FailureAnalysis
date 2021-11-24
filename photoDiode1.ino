7593401e3d5ae05ad5a7cb67a1a4591cb7ad78ed
loganRidings <66134322+loganRidings@users.noreply.github.com>
2021-11-23 17:27:18 -0800

    Update photoDiode1.ino

--- photoDiode1.ino
+++ photoDiode1.ino
@@ -1,6 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
 
+int sensorPin[] = {A0,A1,A2,A3,A4};
+int sensorValue = 0;
+
+void setup() {
+  // Set up serial ports and initialize analog pins
+  Serial.begin(9600);
+  for (int i = 0; i < 5; i++) {
+    pinMode(sensorPin[i], INPUT);
+  }
+  
 }
 
 void loop() {
