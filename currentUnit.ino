224b465cf50a58a771e5bfe20f0ad8d623b10e7a
loganRidings <66134322+loganRidings@users.noreply.github.com>
2021-12-01 10:51:47 -0800

    Update currentUnit.ino

--- currentUnit.ino
+++ currentUnit.ino
@@ -1,5 +1,5 @@
 //resistor vals (Ohms)
-int resistors[5] = {};
+int resistor = 22;
 // Analog pins to read voltage over resistors
 int analogPin[] = {A0,A1,A2,A3,A4};
 
@@ -22,37 +22,37 @@ void loop() {
   String command = Serial.readStringUntil('\n');
   // Above line gets which arduino raspi is asking for
 
-  // Read analog pin for resistor value and send pin no. with voltage drop over resistor
+  // Read analog pin for resistor value and send pin no. with current through resistor
   if (command == unitName) {
-    float voltage = 0;
-    float voltageArray[30] = {};
+    float current = 0;
+    float currentArray[30] = {};
       // Read pins 1-5
       for (int i = 0; i < 5; i++) {
-        // take 30 measurements on analog pin and add to voltageArray
+        // take 30 measurements on analog pin and add to currentArray
         for (int j = 0; j < 30; j++) {
-          voltage = voltageFunc(i);
-          voltageArray[j] = voltage;
+          current = currentFunc(i);
+          currentArray[j] = current;
         }
         // Send the pin number over serial to RasPi
-        Serial.println(i);
+        Serial.println(String(i));
         // Send each voltage reading value independently (as floats), to 4 decimals
         for (int i = 0; i < 30; i ++) {
-          Serial.print(voltageArray[i], 4);
+          Serial.print(String(currentArray[i]));
         }
         // Print newline after data is read over serial send (for each pin read)
-        Serial.println();
+        Serial.print('\n');
       }
   }
 }
 
 /*
   When called, takes pinNum for analog pin address 
-  Returns voltage drop across resistor
+  Returns current through resistor
 */
-float voltageFunc(int pinNum) {
+float currentFunc(int pinNum) {
   int val = 0;
   float voltageRead = 0;
-  val = analogRead(pinNum);
+  val = analogRead(analogPin[pinNum]);
         voltageRead = val * (5.0/1023.0);
-        return voltageRead;
+        return voltageRead/resistors;
 }
